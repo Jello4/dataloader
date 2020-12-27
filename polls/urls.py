df5449339e0d826d1839c11d8a3a4676eb55f3f6
abdullah <abdallahdkhakhni@gmail.com>
2020-12-27 22:35:15 +0300

    3 commit

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('polls', views.index, name='index'),
+    path('', views.index, name='index'),
     path('upload', views.upload_file, name='upload'),
 ]
